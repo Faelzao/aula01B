9b96045000c9175dcc3976a743ceef75d633c26a
Faelzao <rrafa8982@gmail.com>
2023-09-01 13:36:03 +0000

    aulabOFICIAL

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,22 +1,31 @@
 
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import './styles.css';
 
-function App() {
+
+function App(){
+
+  function aoClicar(){
+    alert('Oque tem dentro do input: '+ input)
+  }
+
+  const[input,setInput] = useState ('') 
   return (
     <div className="container">
       <h1 className="title"> Buscador de Cep</h1>
       <div className="containerInput">
-        <input type="text" placeholder="Digite aqui..."/>
+        <input type="text" placeholder="Digite aqui..." value={input} onChange={(e)=> setInput(e.target.value)}/>
+        
 
-        <button className="buttonSearch">
+        <button className="buttonSearch"onClick={aoClicar}>
           <FiSearch size={25} color="#FFF"/>
         </button>
       </div>
       <main className="main">
 <h2>CEP : 6445455</h2>
-<span>Rua: manuel leao rego</span>
-<span>Bairro: xuparina</span>
+<span>Rua: deyde costa </span>
+<span>Bairro: dubay</span>
 
 
 
